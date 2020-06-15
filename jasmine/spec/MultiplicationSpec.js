a68178f370dc4f19376db915022ef2ffe8df6ce3
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-15 22:49:18 +0100

    implementing test of the multiplication function

---
+++ jasmine/spec/MultiplicationSpec.js
@@ -0,0 +1,44 @@
+describe("Multiplication test suite", function() {
+    class MockOperandElement {
+        constructor () {
+            this.innerText = '';
+        }
+    }
+
+    var previousOperandElement;
+    var currentOperandElement;
+    var calculator;
+
+    beforeEach(function() {
+        previousOperandElement = new MockOperandElement();
+        currentOperandElement = new MockOperandElement();
+        calculator = new Calculator(previousOperandElement, currentOperandElement);
+    })
+
+    it('should return 6 if append 2 and 3', function() {
+        calculator.appendNumber(2);
+        calculator.chooseOperation('*');
+        calculator.appendNumber(3);
+        calculator.compute();
+        calculator.updateDisplay();
+        expect(currentOperandElement.innerText).toEqual('6');
+    });
+
+    it('should return 4 if append 1.5 and 3', function() {
+        calculator.appendNumber(1.5);
+        calculator.chooseOperation('*');
+        calculator.appendNumber(3);
+        calculator.compute();
+        calculator.updateDisplay();
+        expect(currentOperandElement.innerText).toEqual('4.5');
+    });
+
+    it('should return 0 when value 0 and 0', function() {
+        calculator.appendNumber(0);
+        calculator.chooseOperation('*');
+        calculator.appendNumber(0);
+        calculator.compute();
+        calculator.updateDisplay();
+        expect(currentOperandElement.innerText).toEqual('0');
+    });
+});
