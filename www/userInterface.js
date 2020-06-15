1908b6b9f3977756d19a9a0d415523d85d68848e
Loyanne Cristine <loyanne.cristine@gmail.com>
2020-06-15 19:26:45 +0100

    refactoring calculator

---
+++ www/userInterface.js
@@ -0,0 +1,39 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {                  // loop over all the different buttons
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();           // will be constantly updated every time when click on a button down
+    })
+})
+
+operationButtons.forEach(button => {                  // loop over all the different buttons
+    button.addEventListener('click', () => {
+        // going to pass the text of the operation that choosing and will update the display
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
